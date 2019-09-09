12f043617c9fdf936268d55cdcd10ab2aff3e5e7
mfreydl <letmeaskmydog@gmail.com>
2019-09-09 09:48:50 -0400

    Implemented TokenHolder on AuthClient responses

--- src/AuthClient.ts
+++ src/AuthClient.ts
@@ -1,7 +1,7 @@
 import * as path from "path"
 import { SingleResult } from "toyhauler-common/dist/contracts";
 import { User, Tenant } from "toyhauler-common/dist/security";
-import axios from "axios"
+import axios, { AxiosResponse } from "axios"
 import { utils } from './utils'
 import { AuthClientConfig } from "./AuthClientConfig";
 import { TokenHolder } from "./tokenHolder";
@@ -48,21 +48,30 @@ export class AuthClient {
             "headers": { "content-type": "application/json" } 
           };
         let apiResponse = await axios.post<SingleResult<User>>(url, req, options); 
-        if (apiResponse.data) {
-          let result = new SingleResult<TokenHolder>(apiResponse.data.success, apiResponse.data.message, apiResponse.data.error, undefined, apiResponse.data.validation);
-          if (apiResponse.data.output) {
-            let token: string|undefined = apiResponse.headers[AuthClient.TOKEN_HEADER_PARAM];
-            if (!token) throw new Error(`Authentication service did not provide a user token.`);
-            result.output = new TokenHolder(apiResponse.data.output, token);
-          }   
-          resolve(result);
-        } else {
-          throw new Error(`Authentication request did not return an output object. HttpStatus: ${apiResponse.status} ${apiResponse.statusText}`);
-        }
+        let result = this.convertToTokenHolderResponse(apiResponse);
+        resolve(result);
       } catch (err) {
         reject(err);
       }
     });
+  }
+
+
+  /** Replaces the user in the output with a TokenHolder so that they client consumer has access to the token.
+   * throws if the axios response does not contain a data property. */
+  private convertToTokenHolderResponse(apiResponse: AxiosResponse<SingleResult<User>>): SingleResult<TokenHolder> {
+    if (apiResponse.data) {
+      // TODO:  Dont construct new - just cast or ducktype and replace the output property.
+      let result = new SingleResult<TokenHolder>(apiResponse.data.success, apiResponse.data.message, apiResponse.data.error, undefined, apiResponse.data.validation);
+      if (apiResponse.data.output) {
+        let token: string|undefined = apiResponse.headers[AuthClient.TOKEN_HEADER_PARAM];
+        if (!token) throw new Error(`Authentication service did not provide a user token.`);
+        result.output = new TokenHolder(apiResponse.data.output, token);
+      }   
+      return result;
+    } else {
+      throw new Error(`Authentication request did not return an output object. HttpStatus: ${apiResponse.status} ${apiResponse.statusText}`);
+    }
   }
 
   /** Provides the ability to extend an existing token.
@@ -70,8 +79,8 @@ export class AuthClient {
    *  Returns any errors encountered refreshing.  
    * @param currentToken The JWT token returned in a prior request.
    * */
-  public async refreshToken(currentToken: string): Promise<SingleResult<User>> {
-    return new Promise<SingleResult<User>>(async (resolve,reject) => {
+  public async refreshToken(currentToken: string): Promise<SingleResult<TokenHolder>> {
+    return new Promise<SingleResult<TokenHolder>>(async (resolve,reject) => {
       try {
         let url = "refreshToken";
         let req = { };
@@ -82,8 +91,9 @@ export class AuthClient {
             [AuthClient.TOKEN_HEADER_PARAM]: currentToken 
           } 
         };
-      let response = await axios.post<SingleResult<User>>(url, req, options);        
-        resolve(response.data);
+        let apiResponse = await axios.post<SingleResult<User>>(url, req, options);
+        let result = this.convertToTokenHolderResponse(apiResponse);
+        resolve(result);
       } catch (err) {
         reject(err);
       }
@@ -96,8 +106,8 @@ export class AuthClient {
    * @param currentToken The JWT token returned in a prior request.
    * @param tenantCode Unique identifier of the tenant being switched to.
    *  Returns any errors encountered refreshing.  */
-  public async switchTenant(currentToken: string, tenantCode: string): Promise<SingleResult<User>> {
-    return new Promise<SingleResult<User>>(async (resolve,reject) => {
+  public async switchTenant(currentToken: string, tenantCode: string): Promise<SingleResult<TokenHolder>> {
+    return new Promise<SingleResult<TokenHolder>>(async (resolve,reject) => {
       try {
         let url = "switchTenant";
         let req = { newTenantCode: tenantCode };
@@ -108,8 +118,9 @@ export class AuthClient {
             [AuthClient.TOKEN_HEADER_PARAM]: currentToken 
           } 
         };
-        let response = await axios.post<SingleResult<User>>(url, req, options);
-        resolve(response.data);
+        let apiResponse = await axios.post<SingleResult<User>>(url, req, options);
+        let result = this.convertToTokenHolderResponse(apiResponse);
+        resolve(result);
       } catch (err) {
         reject(err);
       }
